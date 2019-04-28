0939d6183660709e592de14ac57a3ac8b63eae34
Kevin Wang <kwangsan@gmail.com>
2019-04-27 22:17:32 -0400

    configure Graphene django - add 'graphene_django' to INSTALLED_APPS - add GRAPHENE to settings.py

--- djangographql/settings.py
+++ djangographql/settings.py
@@ -119,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Graphene Schema
+# https://www.howtographql.com/graphql-python/1-getting-started/
+
+GRAPHENE = {
+    'SCHEMA': 'djangographql.schema.schema',
+}
